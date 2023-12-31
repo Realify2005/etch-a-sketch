0319f63b7f15dadf4180762ff36f1410bd3cb5b1
Clement Chau <142649232+Realify2005@users.noreply.github.com>
2024-01-01 01:56:02 +1100

    implement grids

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const GRID_LENGTH = 16;
 
 for (let i = 0; i < GRID_LENGTH; i++) {
     const div = document.createElement("div");
-    div.setAttribute("display: flex;");
+    div.setAttribute("style", "display: flex;");
     const container = document.querySelector(".container");
-    for (let j = 0; j < GRID_LENGTH; i++) {
+    for (let j = 0; j < GRID_LENGTH; j++) {
         const box = document.createElement("div");
-        box.style.margin = "1px solid black"; //delete after
+        box.setAttribute("style", "width: 2em; height: 2em;")
+        box.style.border = "1px solid black"; //delete after
         div.appendChild(box);
     }
     container.appendChild(div);
