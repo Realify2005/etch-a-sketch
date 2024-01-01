7d14c667467d5e39c4729d703e6eb84a2a407eef
Clement Chau <142649232+Realify2005@users.noreply.github.com>
2024-01-01 13:47:12 +1100

    make everything work

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const GRID_LENGTH = 16;
+let grid_length = 16;
+let width = 40 / 16 + "em";
+let height = 40 / 16 + "em";
 
-for (let i = 0; i < GRID_LENGTH; i++) {
+grid_length = parseInt(prompt("What's your desired box side length? Between 0 and 100"))
+if (grid_length > 0 && grid_length <= 100 && !isNaN(grid_length)) {
+    width = 40 / grid_length + "em";
+    height = 40 / grid_length + "em";
+}
+else {
+    alert("Value entered invalid/out of range, proceeding with default side length");
+}
+
+console.log(width);
+
+for (let i = 0; i < grid_length; i++) {
     const div = document.createElement("div");
     div.setAttribute("style", "display: flex;");
     const container = document.querySelector(".container");
-    for (let j = 0; j < GRID_LENGTH; j++) {
+    for (let j = 0; j < grid_length; j++) {
         const box = document.createElement("div");
-        box.setAttribute("style", "width: 2em; height: 2em;")
-        box.style.border = "1px solid black"; //delete after
+        box.setAttribute("style", `width: ${width}; height: ${height};`)
+        box.setAttribute("id", "box");
         div.appendChild(box);
     }
     container.appendChild(div);
 
 }
+
+const boxes = document.querySelectorAll("#box");
+boxes.forEach(box => {
+    box.addEventListener("mouseover", function() {
+        box.style.backgroundColor = "black";
+    })
+})
+
